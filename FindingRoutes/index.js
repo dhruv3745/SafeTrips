b43193c9d26b7c1b37ba76d7d73199726aec8383
3tas <sdhruvemail@gmail.com>
2024-03-03 00:07:17 -0500

    Created node server. added api key and parsing of the data Gives a list of all the coordinates.

---
+++ FindingRoutes/index.js
@@ -0,0 +1,98 @@
+const { request } = require('express');
+const express = require('express');
+const app = express();
+const port = 4444;
+const mapAPIKey = 'AIzaSyDfigUNEOtDjnB3JUnbeBKnajHBXUQE0B8';
+const axios = require('axios');
+const polylineDecoder = "@googlemaps/polyline-codec";
+
+
+
+async function fetchRouteData() {
+  try {
+    const mapsAPI = `https://maps.googleapis.com/maps/api/directions/json?destination=Montreal&origin=Toronto&key=${mapAPIKey}`;
+    const response = await axios.get(mapsAPI);
+    const routeSteps = response.data.routes[0].legs[0].steps;
+    const polyline = [];
+    for (step of routeSteps) {
+        let stepPolyline = step.polyline.points;
+        for (coord of decode(stepPolyline, 5)){
+            polyline.push(coord);
+        }
+    }
+    console.log(polyline);
+    return polyline;
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+}
+
+function decode (encodedPath, precision){
+    const factor = Math.pow(10, precision);
+
+  const len = encodedPath.length;
+
+  // For speed we preallocate to an upper bound on the final length, then
+  // truncate the array before returning.
+  const path = new Array(Math.floor(encodedPath.length / 2));
+  let index = 0;
+  let lat = 0;
+  let lng = 0;
+  let pointIndex = 0;
+
+  // This code has been profiled and optimized, so don't modify it without
+  // measuring its performance.
+  for (; index < len; ++pointIndex) {
+    // Fully unrolling the following loops speeds things up about 5%.
+    let result = 1;
+    let shift = 0;
+    let b = 0;
+    do {
+      // Invariant: "result" is current partial result plus (1 << shift).
+      // The following line effectively clears this bit by decrementing "b".
+      b = encodedPath.charCodeAt(index++) - 63 - 1;
+      result += b << shift;
+      shift += 5;
+    } while (b >= 0x1f); // See note above.
+    lat += result & 1 ? ~(result >> 1) : result >> 1;
+
+    result = 1;
+    shift = 0;
+    do {
+      b = encodedPath.charCodeAt(index++) - 63 - 1;
+      result += b << shift;
+      shift += 5;
+    } while (b >= 0x1f);
+    lng += result & 1 ? ~(result >> 1) : result >> 1;
+
+    path[pointIndex] = [lat / factor, lng / factor];
+  }
+  // truncate array
+  path.length = pointIndex;
+
+//   console.log(path);
+  return path;
+
+}
+
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+
+app.get('/', (req, res) => {
+    console.log("connection accessed");
+    res.send('Connection established :)');
+});
+
+app.get('/api/createRoute/:startLocation/:endLocation', async(req, res) => {
+    const startLocationValue = req.params.startLocation;
+    const endLocationValue = req.params.endLocation;
+    const routeData = await fetchRouteData();
+    res.send(routeData);
+});
+
+
+
